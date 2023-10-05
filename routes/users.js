813d9c1e7cf29bb73acaef7471e5e392acccd2e1
trinhdien <trinhdien606@gmail.com>
2023-10-05 17:08:41 +0700

    dientcph27512 update Authorization

--- routes/users.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const MiddelwareUser = require("../middleware/middleware");
-const ApiUserController = require("../controllers/api.user");
-const multer = require("multer");
-const upload = multer({dest: "uploads/"});
-/* GET users listing. */
-module.exports = router;
