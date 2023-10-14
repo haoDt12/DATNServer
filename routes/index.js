41cfc149b1a27a40724a67ba129fbcf8066bbf37
trinhdien <trinhdien606@gmail.com>
2023-10-14 19:21:05 +0700

    dientcph27512 update ui web

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,21 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/stech.manager/home', function (req, res, next) {
-    res.render('index', {title: 'Danh Sách Sản Phẩm'});
+    res.render('index');
 });
-
+router.get('/stech.manager/product', function (req, res, next) {
+    res.render('product');
+});
+router.get('/stech.manager/category', function (req, res, next) {
+    res.render('category');
+});
+router.get('/stech.manager/login', function (req, res, next) {
+    res.render('login');
+});
+router.get('/stech.manager/add', function (req, res, next) {
+    res.render('addAdmin');
+});
+router.get('/stech.manager/user', function (req, res, next) {
+    res.render('user');
+});
 module.exports = router;
