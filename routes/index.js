6d9aca4be2259b8856d13062431255af44127dce
kttung2609 <kieuthanhtung2k3@gmail.com>
2023-10-27 21:33:27 +0700

    updateProduct

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const ProductModel = require("./../models/model.product");
 
 /* GET home page. */
 router.get('/stech.manager/home', function (req, res, next) {
     res.render('index');
 });
-router.get('/stech.manager/product', function (req, res, next) {
-    res.render('product');
+router.get('/stech.manager/product', async function (req, res, next) {
+    try {
+        let listProduct = await ProductModel.productModel.find();
+        res.render("product", { products: listProduct, message: "get list product success", code: 1 });
+    } catch (e) {
+        console.log(e.message);
+        res.send({ message: "product not found", code: 0 })
+    }
 });
 router.get('/stech.manager/category', function (req, res, next) {
     res.render('category');
@@ -16,6 +23,9 @@ router.get('/stech.manager/login', function (req, res, next) {
 });
 router.get('/stech.manager/add', function (req, res, next) {
     res.render('addAdmin');
+});
+router.get('/stech.manager/addProduct', function (req, res, next) {
+    res.render('addProduct');
 });
 router.get('/stech.manager/user', function (req, res, next) {
     res.render('user');
