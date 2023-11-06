558f2d911128acfe91b4838efc9c5299071a3520
trinhdien <trinhdien606@gmail.com>
2023-11-06 23:39:50 +0700

    update verify otp

--- public/js/verify.js
+++ public/js/verify.js
@@ -70,7 +70,7 @@ document.addEventListener("DOMContentLoaded", function (){
     alert(message)
   }
 
-  verifyButton.addEventListener("submit",function () {
+  verifyButton.addEventListener("click",function () {
     const otp = inputs[0].value + inputs[1].value + inputs[2].value + inputs[3].value + inputs[4].value + inputs[5].value;
     const Uid = getCookieValue("Uid");
     const typeVerify = getCookieValue("typeVerify");
