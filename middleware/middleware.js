154306ae10bc9f332d6ab00208e09fcd1f5f24e2
trinhdien <trinhdien606@gmail.com>
2023-12-16 21:27:07 +0700

    update api thong ke

--- middleware/middleware.js
+++ middleware/middleware.js
@@ -1,25 +1,13 @@
 const jwt = require("jsonwebtoken");
 require("dotenv").config();
 exports.authorizationToken = (req, res, next) => {
-    // const token = req.header('Authorization');
-    // if (!token) {
-    //     return res.send({message: "wrong token", code: 0});
-    // }
-    // try {
-    //     req.data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        next();
-    // } catch (e) {
-    //     return res.send({message: "wrong token", code: 0});
-    // }
-}
-exports.checkPermission = (req,res,next)=>{
     const token = req.header('Authorization');
     if (!token) {
         return res.send({message: "wrong token", code: 0});
     }
     try {
-        let data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
-        return res.send({data: data})
+        req.data = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+        next();
     } catch (e) {
         return res.send({message: "wrong token", code: 0});
     }
