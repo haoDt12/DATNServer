db8c7f200a881b2b6ea4071a3074b86eeabdf2d1
NguyenDucTung <ntung6904@gmail.com>
2023-12-28 12:41:59 +0700

    update api cart v2

--- modelsv2/model.category.js
+++ modelsv2/model.category.js
@@ -1,10 +1,13 @@
 const db = require("../models/database");
-const categorySchema = db.mongoose.Schema({
-    name: {type: String, required: true},
-    img_cover: {type: String, required: true},
-    create_time: {type: String, required: true},
-},{
-    collection:"Categories"
-});
-const categoryModel = db.mongoose.model("categories",categorySchema);
-module.exports = {categoryModel};
+const categorySchema = db.mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    img_cover: { type: String, required: true },
+    create_time: { type: String, required: true },
+  },
+  {
+    collection: "Categories",
+  }
+);
+const categoryModel = db.mongoose.model("categories", categorySchema);
+module.exports = { categoryModel };
